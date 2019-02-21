80c155e4d07f9b2109a47f96d85b6eb5dc342111
杨凯 <906580223@qq.com>
2019-02-21 20:51:46 +0800

    test husky

--- src/views/home.vue
+++ src/views/home.vue
@@ -3,7 +3,7 @@
  * @LastEditors: kai.yang
  * @Description: 首页
  * @Date: 2019-02-19 21:00:02
- * @LastEditTime: 2019-02-21 15:01:51
+ * @LastEditTime: 2019-02-21 20:48:46
  -->
 <template>
   <div class="home">
@@ -44,7 +44,7 @@ export default {
   methods: {
     getMath () {
       debugger
-      ewfwf
+      ewfw
     }
   }
 }
